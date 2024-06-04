00ec1348300ce9dde338874287d7df052eae253d
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2024-06-04 14:36:43 -0400

    wrote the logic for computerChoice

---
+++ myscripts.js
@@ -0,0 +1,34 @@
+
+
+
+/*
+function getComputerChoice(){
+
+ const options = ["rock", "paper", "scissors"];
+
+let randomIndex = Math.floor((Math.random()*options.length)); //returns a random index within the length of options array. Used Math.floor to round the fractional number down to nearest integer, ensuring it fits within valid index range.
+
+let computerOption = options[randomIndex];
+
+
+
+console.log(randomIndex);
+console.log(computerOption);
+
+return computerOption;
+
+
+}*/
+
+//To be deleted wrote them just for testing.
+
+const options = ["rock", "paper", "scissors"];
+
+let randomIndex = Math.floor((Math.random()*options.length)); //returns a random index within the length of options array. Used Math.floor to round the fractional number down to nearest integer, ensuring it fits within valid index range.
+
+let computerOption = options[randomIndex];
+
+
+
+console.log(randomIndex);
+console.log(computerOption);
