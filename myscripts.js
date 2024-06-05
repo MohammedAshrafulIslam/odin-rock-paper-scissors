09177bd4ba9db4b7b3c866709feb54bc5f97735e
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2024-06-05 17:51:04 -0400

    Wrote the logic for deciding the winner after 5 rounds and removed global variables for computer selection and human selection instead used thefunctions as parameters of playRound method

--- myscripts.js
+++ myscripts.js
@@ -4,7 +4,7 @@ function getComputerChoice(){
   let randomIndex = Math.floor((Math.random()*options.length)); //returns a random index within the length of options array. Used Math.floor to round the fractional number down to nearest integer, ensuring it fits within valid index range.
 
   let computerOption = options[randomIndex];
-  console.log("Computer's choice: " + computerOption);
+  
   return computerOption;
   }
 
@@ -22,6 +22,9 @@ function getNewHumanChoice(computerChoice){
  return playRound(computerChoice, newHumanChoice);
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
 
 
 function playRound(computerChoice, humanChoice ){
@@ -33,22 +36,34 @@ function playRound(computerChoice, humanChoice ){
 
  }
  else if( computerChoice === "rock" && humanChoice ==="paper" ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You win, paper beats rock!");
+  humanScore++;
  }
  else if(  computerChoice === "paper" && humanChoice === "rock" ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You lose, paper beats rock");
+  computerScore++;
  }
  else if( computerChoice === "paper" && humanChoice === "scissor" ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You win, scissors cuts paper");
+  humanScore++;
  }
  else if( computerChoice === "scissors" && humanChoice === "paper"  ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You lose, scissors cuts paper");
+  computerScore++;
  }
  else if( computerChoice === "scissors" && humanChoice === "rock"  ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You win, rock beats scissors");
+  humanScore++;
  }
  else if( computerChoice === "rock" && humanChoice === "scissor" ){
+  console.log(`Computer's choice ${computerChoice} and Human's Choice ${humanChoice}`);
   console.log("You lose, rock beats scissors");
+  computerScore++;
  }
  else{
   console.log("Invalid choice, please pick again");
@@ -57,14 +72,33 @@ function playRound(computerChoice, humanChoice ){
 
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-let computerSelection = getComputerChoice();
-let humanSelection = getHumanChoice();
 
-playRound(computerSelection, humanSelection);
 
+
+function playGame() {
+
+//Playing 5 rounds.
+playRound(getComputerChoice(), getHumanChoice());
+playRound(getComputerChoice(), getHumanChoice());
+playRound(getComputerChoice(), getHumanChoice());
+playRound(getComputerChoice(), getHumanChoice());
+playRound(getComputerChoice(), getHumanChoice());
+
+}
+
+function winnerDecision(){
+ if( humanScore < computerScore){
+  console.log ("Computer Won!!");
+ }
+ else{
+  console.log( "You won!!" );
+ }
+}
+
+playGame();
+console.log(`Computer score: ${computerScore}  Human Score: ${humanScore}`);
+winnerDecision();
 
 
 
@@ -77,7 +111,6 @@ const options = ["rock", "paper", "scissors"];
 let randomIndex = Math.floor((Math.random()*options.length)); //returns a random index within the length of options array. Used Math.floor to round the fractional number down to nearest integer, ensuring it fits within valid index range.
 
 let computerOption = options[randomIndex];
-
 
 
 console.log(randomIndex);
